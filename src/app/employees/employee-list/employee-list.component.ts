c0c7c05d7bd86d5c1e1730664ea9b6049024e36a
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2020-03-15 09:49:24 +0530

    Delete function for deleting a record

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { EmployeeService } from './../../shared/employee.service';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/shared/employee.model';
@@ -11,7 +13,10 @@ import { Observable } from 'rxjs';
 export class EmployeeListComponent implements OnInit {
   list: Employee[]
 
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
+    ) { }
 
   ngOnInit() {
     this.service.getEmployees().subscribe(actionArray => {
@@ -26,6 +31,13 @@ export class EmployeeListComponent implements OnInit {
 
   onEdit(emp: Employee){
     this.service.formData = Object.assign({}, emp);
+  }
+
+  onDelete(id: string){
+    if(confirm('Are you sure, you want to delete this record?')){
+      this.firestore.doc('employees/'+id).delete();
+      this.toastr.error('Deleted Successfully', 'Emp. Register')
+    }
   }
 
 }
